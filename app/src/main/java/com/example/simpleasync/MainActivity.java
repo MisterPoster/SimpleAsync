aabb4fbd010c0e9d3627d2176b64ee15c08c0c06
SamuelSparenga <lordnerd999@gmail.com>
2020-03-13 08:52:06 -0700

    working app!

--- app/src/main/java/com/example/simpleasync/MainActivity.java
+++ app/src/main/java/com/example/simpleasync/MainActivity.java
@@ -4,13 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity
 {
     private TextView mText;
     private static final String STATE = "currentText";
+
+    ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,6 +26,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         mText = findViewById (R.id.TextView);
+        pb = findViewById (R.id.progressBar);
+
+        if (savedInstanceState != null)
+        {
+            mText.setText (savedInstanceState.getString(STATE));
+        }
     }
 
     @Override
@@ -32,6 +45,6 @@ public class MainActivity extends AppCompatActivity
     {
         mText.setText (R.string.napping);
 
-        new SimpleAsyncTask (mText).execute ();
+        new SimpleAsyncTask (mText, pb).execute ();
     }
 }   //END OF CLASS
--- app/src/main/java/com/example/simpleasync/SimpleAsyncTask.java
+++ app/src/main/java/com/example/simpleasync/SimpleAsyncTask.java
@@ -1,18 +1,23 @@
 package com.example.simpleasync;
 
 import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import java.lang.*;
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask <Void, Void, String>
+public class SimpleAsyncTask extends AsyncTask <Void, Integer, String>
 {
     private WeakReference<TextView> mText;
+    private WeakReference<ProgressBar> mPb;
+    private static int CHUNK_SIZE = 5;
 
-    SimpleAsyncTask (TextView tv)
+    SimpleAsyncTask (TextView tv, ProgressBar pb)
     {
         mText = new WeakReference<>(tv);
+        mPb = new WeakReference<>(pb);
     }
 
     @Override
@@ -21,18 +26,31 @@ public class SimpleAsyncTask extends AsyncTask <Void, Void, String>
         Random a = new Random ();
 
         int n = a.nextInt (11);
-        int i = n * 200;
+        int mil = n * 200;
 
-        try
+        int chunk_size = mil / CHUNK_SIZE;
+
+        for (int j = 0; j < CHUNK_SIZE; j++)
         {
-            Thread.sleep (i);
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace ();
+            try
+            {
+                Thread.sleep (chunk_size);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace ();
+            }
+
+            publishProgress (((j + 1) * 100) / CHUNK_SIZE);
         }
 
-        return "Finally awake after sleeping " + i + " milliseconds!";
+        return "Finally awake after sleeping " + mil + " milliseconds!";
+    }
+
+    @Override
+    protected void onProgressUpdate (Integer... values)
+    {
+        mPb.get ().setProgress (values[0]);
     }
 
     protected void onPostExecute (String res)
